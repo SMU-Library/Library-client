14ab5355dd1be51d50152b21a6e58791fc742c11
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-02-27 18:51:50 +0100

    created home component and modified exisiting files while adding some other stuff
    npm start works but the page is blank. we gotta work on that

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import  Books  from './books.js';
-import Auth from './auth.js';
-import Issues from './issues.js';
-import Users from './users.js'
+import Auth from './authentication';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const ConfigureStore = ()=>{
