1543549ed8c8aa5d878d861de4948caa4216a0f6
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-02-27 20:28:10 +0100

    Started working on frontend components
    started on the header component, and the usual package.json updates, like newer versions of redux and reactstrap

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,64 @@
+import React,{Component} from 'react';
+import {Navbar, Form, FormGroup, Label, Input, Nav, NavbarToggler,Collapse,NavItem, NavbarBrand, Modal, ModalBody, ModalHeader, Button} from 'reactstrap';
+import {Dropdown,DropdownItem,DropdownMenu,DropdownToggle} from 'reactstrap';
+import { NavLink,Link } from 'react-router-dom';
+import { Control, LocalForm, Errors  } from 'react-redux-form';
+
+
+// should create a Header component as a class first then
+// put render inside of the Header Class
+// Yep, That worked :D
+
+class Header extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+         isNavOpen: false,
+         isModalOpen: false,
+         isRegisterOpen: false,
+         dropdownOpen: false
+        }
+
+        this.toggle = this.toggle.bind(this);
+        this.toggleNav = this.toggleNav.bind(this);
+
+    
+    }
+
+    toggle(){
+        this.setState({dropdownOpen: !this.state.dropdownOpen});
+    }
+
+    toggleNav(){
+        if(window.innerWidth<1200){
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+    }
+
+
+    // ReactStrap (Bootstrap) stuff
+    render(){
+        return (
+            <React.Fragment>
+                <Navbar color="dark" dark expand="xl" fixed="top">
+                    <div className="container">
+                    <NavbarToggler onClick={this.toggleNav}></NavbarToggler>
+                    <NavbarBrand className="mr-auto text-primary" href="/home">
+                    Central Library
+                    </NavbarBrand>
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                    <Nav navbar>
+                        <NavItem className="ml-2" onClick={this.toggleNav}>
+                            <NavLink className="nav-link text-primary" to="/home">
+                            <span className="fa fa-home fa-lg"/> Home
+                        </NavLink>
+                        </NavItem>
+                    </Nav>
+                    </Collapse>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        )}}
